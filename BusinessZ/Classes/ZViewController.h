52902883663d61fbcc4220ede4479e887fca6a6b
wuyanji <wuyanji@o.bike>
2017-10-12 17:03:29 +0800

    1

---
+++ BusinessZ/Classes/ZViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZViewController.h
+//  BusinessZ
+//
+//  Created by wuyanji on 10/11/2017.
+//  Copyright (c) 2017 wuyanji. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZViewController : UIViewController
+
+@end
---
+++ BusinessZ/Classes/ZViewController.m
@@ -0,0 +1,29 @@
+//
+//  ZViewController.m
+//  BusinessZ
+//
+//  Created by wuyanji on 10/11/2017.
+//  Copyright (c) 2017 wuyanji. All rights reserved.
+//
+
+#import "ZViewController.h"
+
+@interface ZViewController ()
+
+@end
+
+@implementation ZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
